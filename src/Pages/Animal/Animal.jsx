99a493d4dc757c24ee699d0c23ab10a6732a7df8
fturannn <furkn.turan@hotmail.com>
2024-03-08 01:35:07 +0300

    Create Customer Page HTTP methods

---
+++ src/Pages/Animal/Animal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Animal() {
+  return (
+    <h1>Animal</h1>
+  )
+}
+
+export default Animal
---
+++ src/Pages/Customer/Customer.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import "./Customer.css"
+
+function Customer() {
+    const [customer, setCustomer] = useState([])
+    const [reload, setReload] = useState(true)
+
+    useEffect(() => {
+        fetch("http://localhost:8080/v1/customers")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            setCustomer(data.data)})
+            setReload(false)
+    }, [reload])
+
+    const handleDelete = (event) => {
+        const id = event.target.id
+        fetch(`http://localhost:8080/v1/customers/${id}`, {method: "DELETE"})
+        .then(() => setReload(true))
+        .catch((error) => {
+            console.error("Error: ", error)
+        })
+    }
+
+  return (<>
+    <h1>Customer</h1>
+    <div className='customer-list'>
+        {customer.map((customer) => {
+            return (
+                <div id={customer.id} onClick={(e) => handleDelete(e)} key={customer.id}>{customer.name}</div>
+            )
+        })}
+    </div>
+    </>
+  )
+}
+
+export default Customer
