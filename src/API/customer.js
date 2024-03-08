b06a7ad627197ef5449f6fb5d892d5ace5741fe7
fturannn <furkn.turan@hotmail.com>
2024-03-09 00:33:02 +0300

    Add save, delete methods

---
+++ src/API/customer.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const getCustomers = async () => {
+    const {data} = await axios.get("http://localhost:8080/v1/customers")
+    return data.data
+}
+
+export const deleteCustomer = async (id) => {
+    const {data} = await axios.delete(`http://localhost:8080/v1/customers/${id}`)
+    return data.data
+}
+
+export const createCustomer = async (customer) => {
+    const {data} = await axios.post("http://localhost:8080/v1/customers", customer)
+    return data.data
+}
--- src/Pages/Customer/Customer.jsx
+++ src/Pages/Customer/Customer.jsx
@@ -1,39 +1,102 @@
-import React, { useState, useEffect } from 'react'
-import "./Customer.css"
+import React, { useState, useEffect } from "react";
+import "./Customer.css";
+import { getCustomers, deleteCustomer, createCustomer } from "../../API/customer";
 
 function Customer() {
-    const [customer, setCustomer] = useState([])
-    const [reload, setReload] = useState(true)
+  const [customer, setCustomer] = useState([]);
+  const [reload, setReload] = useState(true);
+  const [newCustomer, setNewCustomer] = useState({
+    name: "",
+    phone: "",
+    mail: "",
+    address: "",
+    city: "",
+  });
 
-    useEffect(() => {
-        fetch("http://localhost:8080/v1/customers")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            setCustomer(data.data)})
-            setReload(false)
-    }, [reload])
+  useEffect(() => {
+    getCustomers().then((data) => {
+      setCustomer(data);
+    });
+    setReload(false);
+  }, [reload]);
 
-    const handleDelete = (event) => {
-        const id = event.target.id
-        fetch(`http://localhost:8080/v1/customers/${id}`, {method: "DELETE"})
-        .then(() => setReload(true))
-        .catch((error) => {
-            console.error("Error: ", error)
-        })
-    }
+  const handleDelete = (event) => {
+    const id = event.target.id;
+    deleteCustomer(id).then(() => {
+      setReload(true);
+    });
+  };
 
-  return (<>
-    <h1>Customer</h1>
-    <div className='customer-list'>
+  const handleNewCustomer = (event) => {
+    setNewCustomer({
+        ...newCustomer,
+        [event.target.name]: event.target.value,
+
+    })
+  };
+
+  const handleCreate = () => {
+    createCustomer(newCustomer).then(() => {
+        setReload(true)
+    })
+  }
+
+  return (
+    <>
+      <h1>Customer</h1>
+      <div className="customer-inputs">
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={newCustomer.name}
+          onChange={handleNewCustomer}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          name="phone"
+          value={newCustomer.phone}
+          onChange={handleNewCustomer}
+        />
+        <input
+          type="text"
+          placeholder="Mail"
+          name="mail"
+          value={newCustomer.mail}
+          onChange={handleNewCustomer}
+        />
+        <input
+          type="text"
+          placeholder="Address"
+          name="address"
+          value={newCustomer.address}
+          onChange={handleNewCustomer}
+        />
+        <input
+          type="text"
+          placeholder="City"
+          name="city"
+          value={newCustomer.city}
+          onChange={handleNewCustomer}
+        />
+        <button onClick={handleCreate}>Create</button>
+      </div>
+      <div className="customer-list">
         {customer.map((customer) => {
-            return (
-                <div id={customer.id} onClick={(e) => handleDelete(e)} key={customer.id}>{customer.name}</div>
-            )
+          return (
+            <div
+              id={customer.id}
+              onClick={(e) => handleDelete(e)}
+              key={customer.id}
+            >
+              {customer.name}
+            </div>
+          );
         })}
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Customer
+export default Customer;
